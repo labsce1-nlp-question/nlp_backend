924854615a3774caefb0f2f84059c40aa67ffeb1
Daniel Lara <endtrippy@gmail.com>
2019-06-06 09:58:32 -0700

    POST /qa Recieves and tokenizes question. returns answer

--- qa_api/qa_api.py
+++ qa_api/qa_api.py
@@ -1,16 +1,18 @@
 import falcon
 import csv
+import en_core_web_sm
+nlp = en_core_web_sm.load()
 
 with open('Objectives-Master List.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter='\n')
-    
     data = []
+
     for row in readCSV:
         data.append(row)
 
     # Data is reversed simply so viewable print output in terminal relates to first lines of CSV
-    data.reverse()
-    print(data)
+    # data.reverse()
+    # print(data)
 
 class QA:
     def on_get(self, req, resp):
@@ -23,7 +25,13 @@ class QA:
     
     def on_post(self, req, resp):
         """Handles POST requests"""
+        doc = nlp(req.media['question'])
+        tokens = [(w.text, w.pos_) for w in doc]
+        print(tokens)
         
+        answer = {'answer' : 'This is the training kit you were looking for.'}
+        resp.media = answer
+
 
 api = falcon.API()
 api.add_route('/qa', QA())
@@ -32,3 +40,4 @@ api.add_route('/qa', QA())
 # to install needed tools: `pip3 install gunicorn falcon`
 # to run server cd into `qa_api/` and run this command in terminal: `gunicorn --reload -b 0.0.0.0:8000 qa_api:api`
 # you may then make calls to: `localhost:8000/qa`
+# POST request expects to recieve json = {'question': 'your question'} and returns json = {'answer': 'here is your desired training kit'}
