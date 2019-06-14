94f41407ecab46e2954a4c20be2247edc265bb85
Daniel Lara <endtrippy@gmail.com>
2019-06-14 11:50:48 -0700

    Data loaded and cleaned / Bad data segregated Next create searchProfile attribute for each data element with keyword stats and useful info

--- qa_api/qa_api.py
+++ qa_api/qa_api.py
@@ -1,37 +1,48 @@
 import falcon
-import csv
+import json
 import en_core_web_sm
 nlp = en_core_web_sm.load()
 
-with open('Objectives-Master List.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter='\n')
-    data = []
+# Loads Objectives and Modules
+with open ('Objectives.json', 'r') as obj:
+  obj = json.load(obj)
+with open ('Modules.json', 'r') as mod:
+  mod = json.load(mod)
 
-    for row in readCSV:
-        data.append(row)
+# data is made up of Objective:Module pairs
+data = {}
+# lostSouls is made up of unmatchable objectives and modules...
+lostSouls = []
 
-    # Data is reversed simply so viewable print output in terminal relates to first lines of CSV
-    # data.reverse()
-    # print(data)
+# Builds Data
+for o in obj:
+  for m in mod:
+    if o["Modules"] == m["Name"] and o["Modules"] != "" and m["Name"] != "":
+      data[m["Name"]] = {"objective": o, "module": m}
+
+# Harvests Souls
+for o in obj:
+  if o["Modules"] == "":
+    lostSouls.append(o)
+for m in mod:
+  if m["Name"] == "":
+    lostSouls.append(m)
 
 class QA:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        welcome = {
-            "welcome": "I have the answers..."
-        }
+  def on_get(self, req, resp):
+    """Handles GET requests"""
+    welcome = {
+      "welcome": "I have the answers..."
+    }
 
-        resp.media = welcome
+    resp.media = welcome
     
-    def on_post(self, req, resp):
-        """Handles POST requests"""
-        doc = nlp(req.media['question'])
-        tokens = [(w.text, w.pos_) for w in doc]
-        print(tokens)
-        
-        answer = {'answer' : 'This is the training kit you were looking for.'}
-        resp.media = answer
-
+  def on_post(self, req, resp):
+    """Handles POST requests"""
+    doc = nlp(req.media['question'])
+    tokens = [(w.text, w.pos_) for w in doc]
+    answer = data
+    resp.media = answer
 
 api = falcon.API()
 api.add_route('/qa', QA())
